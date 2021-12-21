abd7203599588c3c3ca6e9ffdf545ac286aa952f
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-12-20 19:06:16 -0600

    Installed 'gh-pages' package, added Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../App.css';
+// import gzLogo from "../assets/images/aug21icons.gif";
+
+function Footer() {
+  return (
+    <div>
+        <footer>
+            <h5>&copy;2021 GREG ZARAGOZA</h5>
+            {/* <br> */}
+            <h5>HECHO CON AMOR EN SAN ANTONIO, TX</h5>
+        </footer>
+    </div>
+  );
+}
+
+export default Footer;

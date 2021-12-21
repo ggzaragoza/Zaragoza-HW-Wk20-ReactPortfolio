772fe071f564e15077b2883d25aaf43141ee5f1d
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-12-21 15:07:19 -0600

    Added Projects.js component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from "react";
 import Header from "./components/Header";
 import About from "./components/About";
+import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 // import logo from './logo.svg';
 import './App.css';
+
+const projects = [
+  {
+    id: 1,
+    name: "Rodemap",
+    skills: "Blah blah"
+  },
+  {
+    id: 2,
+    name: "CodeFront",
+    skills: "Sum sum"
+  },
+  {
+    id: 3,
+    name: "The Quiz Template",
+    skills: "Got skillz brah"
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <Header />
       <About />
+      <Projects projects={projects} />
       <Footer />
     </div>
   );

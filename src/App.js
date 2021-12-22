de08568cf90e8494a7635623b4f29f60b7fc6694
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-12-22 01:02:31 -0600

    Implemented Contact.js with rerouted icons, edited flex attributes for additional project renderings

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 // import logo from './logo.svg';
 import './App.css';
@@ -30,7 +31,31 @@ const projects = [
     github: "https://github.com/ggzaragoza/Zaragoza-HW-Wk4-CodeQuiz",
     live: "https://ggzaragoza.github.io/Zaragoza-HW-Wk4-CodeQuiz/",
     background: process.env.PUBLIC_URL + "/assets/images/codequiz.png"
-  }
+  },
+  {
+    id: 4,
+    name: "Rodemap",
+    skills: "Google Maps API",
+    github: "https://github.com/Baiterade/ConcertMaps",
+    live: "https://baiterade.github.io/ConcertMaps/",
+    background: process.env.PUBLIC_URL + "/assets/images/rodemap.png"
+  },
+  {
+    id: 5,
+    name: "CodeFront",
+    skills: "Server-Side API, JSON",
+    github: "https://github.com/ggzaragoza/Zaragoza-HW-Wk6-WeatherDashboard",
+    live: "https://ggzaragoza.github.io/Zaragoza-HW-Wk6-WeatherDashboard/",
+    background: process.env.PUBLIC_URL + "/assets/images/codefront.jpg"
+  },
+  {
+    id: 6,
+    name: "The Quiz Template",
+    skills: "JavaScript, DOM API",
+    github: "https://github.com/ggzaragoza/Zaragoza-HW-Wk4-CodeQuiz",
+    live: "https://ggzaragoza.github.io/Zaragoza-HW-Wk4-CodeQuiz/",
+    background: process.env.PUBLIC_URL + "/assets/images/codequiz.png"
+  },
 ];
 
 function App() {
@@ -39,6 +64,7 @@ function App() {
       <Header />
       <About />
       <Projects projects={projects} />
+      <Contact />
       <Footer />
     </div>
   );
---
+++ src/components/Contact.js
@@ -0,0 +1,53 @@
+import React from "react";
+import '../App.css';
+import phoneIcon from "../assets/images/phone.png";
+import asterisk from "../assets/images/email.png";
+import githubLogo from "../assets/images/github.png";
+import linkedinLogo from "../assets/images/linkedin.png";
+import igLogo from "../assets/images/ig.png";
+import dropboxLogo from "../assets/images/dropbox.png";
+
+function Contact() {
+  return (
+    // <div>
+    //     <header>
+    //     <ul>
+    //         <li> <a href="#work">Projects</a> </li>
+    //     </ul>
+        
+    //     <img src={gzLogo} alt="Rotating gif of GZ initial icons"/>
+
+    //     <ul>
+    //         <li> <a href="#contact">Contact</a> </li>
+    //     </ul>
+    //     </header>
+    // </div>
+    <section id="contact">
+
+    <div id="address">
+        <div id="phone">
+            <img src={phoneIcon} alt="Keypad icon"/>
+            <p>210 833 1674</p>
+        </div>
+        <div>
+            <img src={asterisk} alt="At symbol"/>
+            <p>greg.g.zaragoza@gmail.com</p>
+        </div>
+    </div>
+
+    <div id="socials">
+        <a href="https://www.github.com/ggzaragoza/"><img href="https://www.github.com/ggzaragoza/" src={githubLogo} alt="Github icon"/></a>
+        <a href="https://www.linkedin.com/in/gregzaragoza/"><img href="https://www.linkedin.com/in/gregzaragoza/" src={linkedinLogo} alt="LinkedIn icon"/></a>
+        <a href="https://www.instagram.com/gregzaragoza/"><img href="https://www.instagram.com/gregzaragoza/" src={igLogo} alt="Instagram icon"/></a>
+        <a href="https://www.dropbox.com/s/wecoeuqmdpty3ej/Greg%20Zaragoza%20-%20Sep.%202021%20Resume.pdf?dl=0">
+            <img href="https://www.dropbox.com/s/wecoeuqmdpty3ej/Greg%20Zaragoza%20-%20Sep.%202021%20Resume.pdf?dl=0"
+            src={dropboxLogo}
+            alt="Dropbox icon"/>
+        </a>
+    </div>
+
+</section>
+  );
+}
+
+export default Contact;
